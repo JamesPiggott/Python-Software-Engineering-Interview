d3c602cff69be4c79100d1e58ccafe98a2a02d52
JamesPiggott <jjhpiggott@gmail.com>
2018-05-10 09:47:46 +0200

    Merge sort implementation

--- Sort/Mergesort.py
+++ Sort/Mergesort.py
@@ -1,7 +1,7 @@
 '''
-  Bubblesort.
+  Mergesort.
 
-  This is the Python implementation of the Bubblesort sorting algorithm as it applies to an array of Integer values.
+  This is the Python implementation of the Mergesort sorting algorithm as it applies to an array of Integer values.
 
   Running time: ?
   Data movement: ?
@@ -13,37 +13,54 @@
 import sys
 import timeit
 
-class Bubblesort(object):
+class Mergesort(object):
 
     def __init__(self):
         print()
 
-    def bubblesort(self, unsorted):
+    def mergesort(self, unsorted):
+        if len(unsorted) > 1:
+            mid = len(unsorted)//2
+            leftHalf = unsorted[:mid]
+            rightHalf = unsorted[mid:]
 
-        endinnerLoop = len(unsorted)
-        steps = 0
+            self.mergesort(leftHalf)
+            self.mergesort(rightHalf)
 
-        for i in range(0, len(unsorted)):
-            for j in range(1, endinnerLoop):
-                if unsorted[j-1] > unsorted[j]:
-                    swap = unsorted[j]
-                    unsorted[j] = unsorted[j - 1]
-                    unsorted[j - 1] = swap
-                steps = steps + 1
-                endinnerLoop = endinnerLoop - 1
-        return unsorted
+            i = 0
+            j = 0
+            k = 0
 
+            while i < len(leftHalf) and j < len(rightHalf):
+                if leftHalf[i] < rightHalf[j]:
+                    unsorted[k] = leftHalf[i]
+                    i = i + 1
+                else:
+                    unsorted[k] = rightHalf[j]
+                    j = j + 1
+                k = k + 1
+
+            # Now deal with the residual parts of the sub-lists, they should already be sorted
+            while i < len(leftHalf):
+                unsorted[k] = leftHalf[i]
+                i = i + 1
+                k = k + 1
+
+            while j < len(rightHalf):
+                unsorted[k] = rightHalf[j]
+                j = j + 1
+                k = k + 1
 
 def main():
     unsorted = [7, 3, 8, 2, 1, 9, 4, 6, 5, 0]
 
-    bubble = Bubblesort()
+    merge = Mergesort()
 
-    bubble.bubblesort(unsorted)
+    merge.mergesort(unsorted)
 
     print(unsorted)
 
 if __name__ == "__main__":
-    print(timeit.timeit("main()", setup="from __main__ import Bubblesort"))
+    #print(timeit.timeit("main()", setup="from __main__ import Mergesort"))
 
-    # main()
+    main()
