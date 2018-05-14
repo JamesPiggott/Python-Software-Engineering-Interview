9a0b8e227b1055e2d0f7ec2716c605ec61295af4
JamesPiggott <jjhpiggott@gmail.com>
2018-05-14 20:34:38 +0200

    Merge sort implementation

---
+++ Sort/Insertionsort.py
@@ -0,0 +1,41 @@
+'''
+  Insertionsort.
+
+  This is the Python implementation of the Insertionsort sorting algorithm as it applies to an array of Integer values.
+
+  Running time: ?
+  Data movement: ?
+
+  @author James Piggott.
+
+'''
+
+import sys
+import timeit
+
+class Insertionsort(object):
+
+    def __init__(self):
+        print()
+
+    def insertionsort(self, unsorted):
+        print()
+
+        for i in range(0, len(unsorted)):
+
+
+
+
+def main():
+    unsorted = [7, 3, 8, 2, 1, 9, 4, 6, 5, 0]
+
+    insert = Insertionsort()
+
+    insert.insertionsort(unsorted)
+
+    print(unsorted)
+
+if __name__ == "__main__":
+    # print(timeit.timeit("main()", setup="from __main__ import Insertionsort"))
+
+    main()
