5398950a75db6e421d5ed22438259b487c15d7a3
JamesPiggott <jjhpiggott@gmail.com>
2018-05-22 20:39:56 +0200

    Insertion sort

--- Sort/Insertionsort.py
+++ Sort/Insertionsort.py
@@ -19,12 +19,12 @@ class Insertionsort(object):
         print()
 
     def insertionsort(self, unsorted):
-        print()
-
         for i in range(0, len(unsorted)):
-
-
-
+            for j in range(unsorted[i], 0, -1):
+                if unsorted[j] < unsorted[j - 1]:
+                    swap = unsorted[j - 1]
+                    unsorted[j - 1] = unsorted[j]
+                    unsorted[j] = swap
 
 def main():
     unsorted = [7, 3, 8, 2, 1, 9, 4, 6, 5, 0]
