724ae20710487d6a4657fea362a9f8b03d311432
JamesPiggott <jjhpiggott@gmail.com>
2018-05-24 22:43:08 +0200

    Added more Numpy examples

--- Numpy/NumpyArray.py
+++ Numpy/NumpyArray.py
@@ -31,6 +31,7 @@ def simplePythonArrays():
     # Finally we can also select the increment used choose elements
     print("Third range: ", x[0:5:2])  # Increment 2 skips the second and fourth elements
 
+
 def simpleNumpy():
 
     # Same one-dimensional array as above
@@ -59,18 +60,30 @@ def simpleNumpy():
     # Numpy will try to divide the range into equal part to get right number of elements
     print("Linspace array:", np.linspace(0, 3, 8))
 
+
 def linearAlgebraWithNumpy():
-    print()
-    # shape
 
-    # reshape
+    a = np.array([1, 2, 5, 10, 30, 42])
+    b = np.array([[1, 2, 5.5, 10], [30, 42, 86]])
 
-    #
+    # With shape the size of an Numpy array can be retrieved
+    print("Array a has size: ", a.shape)  # A has 7 elements so shape is 7
+    print("Array b has size: ", b.shape)  # B has 2 elements (2 arrays) so shape is 2
+
+    # With reshape the shape of array can be changed without affecting the data (such as the transpose)
+    c = np.reshape(a, (3, 2))
+    print("Now a is a 3 by 2 matrix", c)
+
+    # If we are not certain about the shape we can use a placeholder value '-1'
+    d = np.reshape(a, (3, -1))
+    print("Now a is a 3 by 2 matrix", d)
 
 
 def advancedNumpy():
-    print()
-    # ndarray
+
+    # With ndarray we can fully customize an Numpy array. Lets see what a Christmas tree implementation looks like
+    example = np.ndarray(shape=(3, 2), dtype=float, order='F')
+    print(example)
 
     # argmax
 
@@ -80,9 +93,10 @@ def plottingNumpyArrays():
     plt.show()
 
 
-
 if __name__ == "__main__":
 
     simplePythonArrays()
     simpleNumpy()
-    plottingNumpyArrays()
+    # plottingNumpyArrays()
+    linearAlgebraWithNumpy()
+    advancedNumpy()
